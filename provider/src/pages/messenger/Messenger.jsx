44efb9d9ac695a72bf829eb8b5ba5b275acc2707
Juan <juanpablo_1810@hotmail.com>
2021-12-01 16:09:58 -0500

    main

--- provider/src/pages/messenger/Messenger.jsx
+++ provider/src/pages/messenger/Messenger.jsx
@@ -24,6 +24,20 @@ export default function Messenger({ username }) {
   const { user } = useContext(AuthContext);
 
   useEffect(() => {
+    const fetchPosts = async () => {
+      const res = username
+        ? await axios.get("/posts/profile/" + username)
+        : await axios.get("posts/timeline/" + user._id);
+      setPosts(
+        res.data.sort((p1, p2) => {
+          return new Date(p2.createdAt) - new Date(p1.createdAt);
+        })
+      );
+    };
+    fetchPosts();
+  }, [username, user._id]);
+
+  useEffect(() => {
     socket.current = io("ws://localhost:8900");
     socket.current.on("getMessage", (data) => {
       setArrivalMessage({
